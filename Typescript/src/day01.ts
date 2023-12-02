a34fec65e7358516f64e16a467d481927753660a
matthiasffm <matthias@schmidt-dot.net>
2023-12-02 04:17:30 +0100

    [feat] solved day 1 in typescript

---
+++ Typescript/src/day01.ts
@@ -0,0 +1,58 @@
+// The Elves discover that their calibration document has been amended by a very young Elf who was apparently just excited to show off her art skills. Consequently, the Elves
+// are having trouble reading the values on the document. The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration
+// value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single
+// two-digit number.
+//
+// Puzzle == Consider your entire calibration document. What is the sum of all of the calibration values?
+export function puzzle1(lines: Array<string>) : number {
+    return lines.reduce((sum, line) => sum + calibrationValue(line, reverse(line)), 0);
+}
+
+// Your calculation isn't quite right. It looks like _some_ of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine _also_
+// count as valid "digits".
+// Equipped with this new information, you now need to find the real first and last digit on each line.
+//
+// Puzzle == What is the sum of all of the calibration values?
+export function puzzle2(lines: Array<string>) : number {
+    return lines.reduce((sum, line) => sum + calibrationValue(normalize(line), normalizeReverse(line)), 0);
+}
+
+function calibrationValue(lineL2R: string, lineR2L: string) : number {
+    return mapDigits(lineL2R)[0] * 10 + mapDigits(lineR2L)[0];
+}
+
+function mapDigits(line : string) : number[] {
+    return line.split('')
+               .filter(c => isDigit(c))
+               .map(c => parseInt(c[0]));
+}
+
+function isDigit(c: string) : boolean {
+    return c[0] >= '0' && c[0] <= '9';
+}
+
+const digitMapping : Map<string, string> = new Map([
+    ["one",     "1" ],
+    ["two",     "2" ],
+    ["three",   "3" ],
+    ["four",    "4" ],
+    ["five",    "5" ],
+    ["six",     "6" ],
+    ["seven",   "7" ],
+    ["eight",   "8" ],
+    ["nine",    "9" ]
+]);
+
+// normalizes the string by replacing all occurences of digits as word by their digit characters from left to right
+function normalize(line: string) : string {
+    return line.replace(/one|two|three|four|five|six|seven|eight|nine/gi, matched => digitMapping.get(matched)!);
+}
+
+// normalizes the string by replacing all occurences of digits as word by their digit characters from right to left
+function normalizeReverse(line: string) : string {
+    return reverse(line).replace(/eno|owt|eerht|ruof|evif|xis|neves|thgie|enin/gi, matched => digitMapping.get(reverse(matched))!);
+}
+
+function reverse(s : string) : string {
+    return s.split("").reverse().join("");
+}
---
+++ Typescript/tests/day01.test.ts
@@ -0,0 +1,26 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+import { puzzle1, puzzle2 } from '../src/day01';
+
+describe('solving day01', () => {
+    test('puzzle1 should return the correct sum for the sample calibration data', () => {
+        expect(puzzle1(["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet" ]))
+                .toBe(12 + 38 + 15 + 77);
+    });
+
+    test('puzzle2 should return the correct sum for the sample calibration data', () => {
+        expect(puzzle2(["two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen" ]))
+                .toBe(29 + 83 + 13 + 24 + 42 + 14 + 76);
+    });
+
+    const data = readFileSync(join(__dirname, '../../.input/day01.data'), 'utf-8').split('\n');
+
+    test('puzzle1 should return the correct sum for the real AoC calibration data', () => {
+        expect(puzzle1(data)).toBe(55816);
+    });
+
+    test('puzzle2 should return the correct sum for the real AoC calibration data', () => {
+        expect(puzzle2(data)).toBe(54980);
+    });
+});
