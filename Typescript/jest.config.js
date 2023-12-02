453ba91819878dfd79c3d0bbeccae7e59dd57163
matthiasffm <matthias@schmidt-dot.net>
2023-12-02 10:03:13 +0100

    [ci] missing jtest.config file added

---
+++ Typescript/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    transform: { '^.+\\.(ts|tsx)$': 'ts-jest', },
+    testEnvironment: 'node',
+    testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+};
